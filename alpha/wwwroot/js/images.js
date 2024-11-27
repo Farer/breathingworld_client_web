03a8f7ec22fcef8d281ddbfcd87a09f6efc44c2f
Farer <farer@hotmail.com>
2024-11-27 17:11:39 +0900

    change image

--- alpha/wwwroot/js/images.js
+++ alpha/wwwroot/js/images.js
@@ -3,7 +3,7 @@ const Images = {
     LoadedCount: 0,
     PreloadData: [
         'weed|/img/sprites/sprite_weed_512_tiny.png',
-        'grass|/img/sprites/sprite_grass_512_tiny.png',
+        'grass|/img/sprites/sprite_grass_512_tiny.png?1',
         'dirt_floor|/img/continous_dirt_floor_128x128_bright_tiny.png',
         'rabbit|/img/sprites/sprite_rabbit_256_tiny.png',
         'rabbit_etc|/img/sprites/sprite_rabbit_etc_512_tiny.png?1',
