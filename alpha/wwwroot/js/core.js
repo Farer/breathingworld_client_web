50e2dfd1d1cdb2e9d9a9146db023b71a50237405
Farer <farer@hotmail.com>
2025-04-09 11:43:13 +0900

    added reddit link

--- alpha/wwwroot/js/core.js
+++ alpha/wwwroot/js/core.js
@@ -104,6 +104,9 @@ const Core = {
             html += '<a href="https://github.com/Farer/breathingworld_client_web" target="_blank" style="display: block;">';
                 html += '<img src="' + window.cdnPrefix + '/img/github-mark-white.svg" alt="Github" style="width: 30px; height: 30px; '+filterShadowStyle+'">';
             html += '</a>';
+            html += '<a href="https://www.reddit.com/r/BreathingWorld/" target="_blank" style="display: block;">';
+                html += '<img src="' + window.cdnPrefix + '/img/icon_reddit_950_tiny.png" alt="Reddit" style="width: 30px; height: 30px; '+filterShadowStyle+'">';
+            html += '</a>';
             html += '<a href="https://discord.gg/4Y2TpWDtJm" target="_blank" style="display: block;">';
                 html += '<img src="' + window.cdnPrefix + '/img/icon_clyde_white_RGB.svg" alt="Discord" style="width: 30px; height: 30px; '+filterShadowStyle+'">';
             html += '</a>';
