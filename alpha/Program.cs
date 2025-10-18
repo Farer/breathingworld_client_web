42cf0a0e3821061d8a2191832d7d2bf0a91422ea
Farer <farer@hotmail.com>
2025-10-18 18:57:55 +0900

    Update Program.cs

--- alpha/Program.cs
+++ alpha/Program.cs
@@ -1,14 +1,24 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.StaticFiles;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+// 🔧 Firefox fix: add KTX2 MIME type
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings[".ktx2"] = "image/ktx2";
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.UseRouting();
 app.MapControllers();
 app.UseDefaultFiles();
-app.UseStaticFiles();
+
+// ✅ apply the custom provider here
+app.UseStaticFiles(new StaticFileOptions
+{
+    ContentTypeProvider = provider
+});
 
 app.Run("http://0.0.0.0:9000");
